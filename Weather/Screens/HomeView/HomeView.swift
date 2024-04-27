b022400828ace554e29a0e40f4147ca6b69c053a
vlvsv <vvs_2015@icloud.com>
2024-04-27 16:14:37 +0300

    Initial Commit

---
+++ Weather/Screens/HomeView/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Weather
+//
+//  Created by Vlad V on 27.04.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Vlad V on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
